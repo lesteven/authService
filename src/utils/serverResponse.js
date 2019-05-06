fb58808bc4e578610481bc3e5d0866ebe5da62fe
lesteven <le_steven@outlook.com>
2019-05-06 00:32:59 -0700

    feat: add generic serverResponses

---
+++ src/utils/serverResponse.js
@@ -0,0 +1,31 @@
+const debug = require('debug')('http');
+
+// responses return function in order to handle errors
+
+
+function sendError(res, status, message) {
+  return res.status(status).json({
+    status,
+    error: message,
+  });
+};
+
+function sendErrorCB(res, status, message) {
+  return ((err) => {
+    debug(err);
+    return sendError(res, status, message);
+  });
+};
+
+function sendSuccess(res, status, data) {
+  return res.status(status).json({
+    status,
+    data
+  });
+};
+
+module.exports = {
+  sendError: sendError,
+  sendErrorCB: sendErrorCB,
+  sendSuccess: sendSuccess,
+}
