f10f87d1aaf1ad928c3b290f37fb85be944b729d
lesteven <le_steven@outlook.com>
2019-05-28 15:28:16 -0700

    test: update login tests

--- test/intTest/login.test.js
+++ test/intTest/login.test.js
@@ -1,41 +1,54 @@
 const app = require('../../src/app');
 const request = require('supertest');
 const {
-  insertUser
+  insertUser,
+  deleteUser
 } = require('../../src/utils/passportQueries');
 
-const data = {
+const existUser = {
+  username: 'helloExists',
+  password: 'adasdas'
+}
+
+const existWrong = {
+  username: 'helloExists',
+  password: 'wrongpw'
+}
+
+const newUser = {
   username: 'hello',
   password: 'lala123',
 }
 
+beforeAll(() => {
+  insertUser(existUser);
+})
+
+afterEach(() => {
+  deleteUser(newUser);
+});
 
 
 describe('login service', () => {
-  /*
   it('log user in if correct user and pw', () => {
     return request(app)
       .post('/api/sessions')
-      .send(data)
+      .send(existUser)
       .set('Accept', 'application/json')
-      .expect(200)
+      .expect(201)
   });
   it('send error if user doesnt exist', () => {
     return request(app)
       .post('/api/sessions')
-      .send(data)
+      .send(newUser)
       .set('Accept', 'application/json')
       .expect(400)
   });
   it('send error if incorrect password', () => {
     return request(app)
       .post('/api/sessions')
-      .send(data)
+      .send(existWrong)
       .set('Accept', 'application/json')
       .expect(400)
-  });
-  */
-  it('',() => {
-
   });
 });
